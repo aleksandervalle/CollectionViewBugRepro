174b80d94d09b124db7f713607739ab10bbb7145
Aleksander Valle Grunnvoll <aleksander.grunnvoll@poweroffice.no>
2024-01-24 14:44:04 +0100

    first

---
+++ CollectionViewBugRepro/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace CollectionViewBugRepro
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool _isRefreshing;
+        public bool IsRefreshing
+        {
+            get => _isRefreshing;
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+        public ICommand RefreshCommand { get; set; }
+
+        public ObservableCollection<ListItem> MyList { get; set; } = new ObservableCollection<ListItem>()
+        {
+            new First(),
+            new TheRest(),
+            new TheRest(),
+            new TheRest(),
+        };
+
+        public MainPage()
+        {
+            RefreshCommand = new Command(async () => await RefreshItemsAsync());
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private async Task RefreshItemsAsync()
+        {
+            try
+            {
+                IsRefreshing = true;
+
+                await Task.Delay(2000);
+                IsRefreshing = false;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ CollectionViewBugRepro/MyTemplateSelector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewBugRepro
+{
+    public interface ListItem
+    {
+        string Text { get; }
+    }
+
+    public class First : ListItem
+    {
+        public string Text => "First";
+    }
+
+    public class TheRest : ListItem
+    {
+        public string Text => "The Rest";
+    }
+
+    public class MyTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TemplateOne { get; set; }
+        public DataTemplate TemplateTwo { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is First)
+            {
+                return TemplateOne;
+            }
+            else
+            {
+                return TemplateTwo;
+            }
+        }
+    }
+
+}
